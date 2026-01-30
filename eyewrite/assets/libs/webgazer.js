a6da61c8bd31d58e6946bbf393231f48a3bcd198
ElishaParker <elishaparker@hotmail.com>
2026-01-30 11:42:48 -0500

    Upload all eyewrite files

---
+++ eyewrite/assets/libs/webgazer.js
@@ -0,0 +1,8 @@
+// Placeholder – replace with full WebGazer build when integrating gaze tracking
+window.webgazer={
+  begin:()=>console.warn("webgazer.begin() placeholder"),
+  setGazeListener:()=>{},
+  clearGazeListener:()=>{},
+  showVideo:()=>{},
+  showPredictionPoints:()=>{}
+};
