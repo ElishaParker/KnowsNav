06e39972af8b785b16735e73a97d028c6a38f60b
ElishaParker <elishaparker@hotmail.com>
2026-01-30 11:41:21 -0500

    Delete eyewrite directory

--- eyewrite/assets/libs/webgazer.js
+++
@@ -1,8 +0,0 @@
-// Placeholder – replace with full WebGazer build when integrating gaze tracking
-window.webgazer={
-  begin:()=>console.warn("webgazer.begin() placeholder"),
-  setGazeListener:()=>{},
-  clearGazeListener:()=>{},
-  showVideo:()=>{},
-  showPredictionPoints:()=>{}
-};
